2944288c22b63d5241449e848c29b409ea66e3d2
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-05-10 14:27:19 +0530

    Smoothen curve

--- Files/plot.jl
+++ Files/plot.jl
@@ -5,7 +5,7 @@ n = size(keys(data1.colindex),1)-1
 cols = distinguishable_colors(n, colorant"black")
 algos = keys(data1.colindex)[2:end]
 Gadfly.set_default_plot_size(12inch, 6inch)
-p=Gadfly.plot([layer(data1, y=algos[i],x="CacheSize", Geom.line, Theme(default_color=cols[i],line_width=.8mm)) for i=1:n]..., Guide.XLabel("Cache Size"),
+p=Gadfly.plot([layer(data1, y=algos[i],x="CacheSize", Geom.smooth, Theme(default_color=cols[i],line_width=.8mm)) for i=1:n]..., Guide.XLabel("Cache Size"),
 Guide.YLabel("Hit Ratio"),
 Guide.Title("Cache Size vs Cache Hit Ratio"),
 Guide.manual_color_key("Legend", [replace(string(i),"_",".") for i in algos], cols))
